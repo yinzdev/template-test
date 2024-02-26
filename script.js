ce39a08a763d22d97f5e618b63878f5e2b7aa6da
yinzdev <160644144+yinzdev@users.noreply.github.com>
2024-02-26 17:07:18 -0300

    quote generator

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const quoteContainer = document.getElementById('quote-generator');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('quote-container');
 
+// Show New Quote
+function newQuote() {
+  // Local Quotes
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  // console.log(quote);
+
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  quoteText.textContent = quote.text;
+}
+
+newQuote();
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
